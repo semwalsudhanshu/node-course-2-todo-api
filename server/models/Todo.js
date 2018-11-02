40ea69d69276810603869d3432fed7deafd9589e
semwalSudhanshu <semwal.sudhanshu@gmail.com>
2018-11-02 12:39:00 +0530

    ADDED POST TODOS AND REFACTOR MONGOOSE

---
+++ server/models/Todo.js
@@ -0,0 +1,19 @@
+var mongoose=require('mongoose');
+var Todo = mongoose.model('Todo',{
+  text:{
+    type:String,
+    required:true,
+    minlength:1,
+    trim:true
+  },
+  completed:{
+    type:Boolean,
+    default:false
+  },
+  completedat:{
+     type:Number,
+     default:null
+  }
+});
+
+module.exports={Todo:Todo};
---
+++ server/server.js
@@ -0,0 +1,62 @@
+var express=require('express');
+var bodyParser=require('body-parser');
+
+
+
+var {mongoose}=require('./db/mongoose.js');
+var {Todo}=require('./models/Todo.js');
+var {User}=require('./models/User.js');
+
+
+var app=express();
+app.use(bodyParser.json());
+
+app.post('/todos',(req,res)=>{
+  var todo =new Todo({
+    text:req.body.text
+  });
+  todo.save().then((doc)=>{
+    res.send(doc);
+  },(e)=>{
+    res.status(400).send(e);
+  });
+});
+
+
+
+app.listen(3000,()=>{
+
+  console.log('Server started on port 3000');
+});
+
+
+ // var newTodo =new Todo({text:'Cook dinner'});
+ // newTodo.save().then((doc)=>{
+ //   console.log('Saved Todo',doc);
+ // },(e)=>{
+ //   console.log('Unable to save Todo');
+ // })
+
+
+
+
+ // var OtherTodo=new Todo({
+ //   text:'        Edit the video          '
+ // });
+ // OtherTodo.save().then((doc)=>{
+ //    console.log('Saved Todo ',JSON.stringify(doc,undefined,2));
+ // },(e)=>{
+ //   console.log('Unable to save the Todo ',e);
+ // });
+
+
+//
+//  var newuser=new User({
+//    text:'example@gmail.com'
+//  });
+// newuser.save().then((doc)=>{
+//   console.log('SAVED USER',doc);
+// },(e)=>{
+//   console.log('Unable to save the user');
+// });
+// //save new Something
